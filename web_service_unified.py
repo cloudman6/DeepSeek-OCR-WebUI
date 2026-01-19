2fa9a2cabacaa4b34e0e8171bd2ff189dd82caf9
cloudman6 <CloudMan6@foxmail.com>
2026-01-19 15:56:21 +0800

    Fix CORS x-client-id Heaa

--- web_service_unified.py
+++ web_service_unified.py
@@ -255,7 +255,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "x-client-id", "x-forwarded-for"],
 )
 
 # Define frontend path
